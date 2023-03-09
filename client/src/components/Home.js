53b0b41e24741a690759f4a81ffa056abb4ef785
Dhison <dhisonp@gmail.com>
2023-03-08 22:37:01 -0500

    Initial deployment.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import Loading from "./Loading";
 
 const Home = () => {
+    // Form Data
     const [fullName, setFullName] = useState("");
     const [currentPosition, setCurrentPosition] = useState("");
     const [currentLength, setCurrentLength] = useState(1);
     const [currentTechnologies, setCurrentTechnologies] = useState("");
     const [headshot, setHeadshot] = useState(null);
+    const [companyInfo, setCompanyInfo] = useState([
+        { name: "", position: "" },
+    ]);
+    // Utility
     const [loading, setLoading] = useState(false);
 
+    // Handlers
     const handleFormSubmit = (e) => {
         e.preventDefault();
         console.log({
@@ -20,73 +26,140 @@ const Home = () => {
         });
         setLoading(true);
     };
-    //👇🏻 Renders the Loading component you submit the form
+
+    const handleAddCompany = () =>
+        setCompanyInfo([...companyInfo, { name: "", position: "" }]);
+
+    const handleRemoveCompany = (index) => {
+        const list = [...companyInfo];
+        list.splice(index, 1);
+        setCompanyInfo(list);
+    };
+
+    const handleUpdateCompany = (e, index) => {
+        const { name, value } = e.target;
+        const list = [...companyInfo];
+        list[index][name] = value;
+        setCompanyInfo(list);
+    };
+
     if (loading) {
         return <Loading />;
     }
+
     return (
-        <div className='app'>
-            <h1>Resume Builder</h1>
-            <p>Generate a resume with ChatGPT in few seconds</p>
+        <div className="app">
+            <h1>Resume Genie</h1>
+            <p>Tell Mr. AI to do a resume quickie</p>
             <form
                 onSubmit={handleFormSubmit}
-                method='POST'
-                encType='multipart/form-data'
+                method="POST"
+                encType="multipart/form-data"
             >
-                <label htmlFor='fullName'>Enter your full name</label>
+                <label htmlFor="fullName">Enter your full name here.</label>
                 <input
-                    type='text'
+                    type="text"
                     required
-                    name='fullName'
-                    id='fullName'
+                    name="fullName"
+                    id="fullName"
                     value={fullName}
                     onChange={(e) => setFullName(e.target.value)}
                 />
-                <div className='nestedContainer'>
+                <div className="nestedContainer">
                     <div>
-                        <label htmlFor='currentPosition'>Current Position</label>
+                        <label htmlFor="currentPosition">
+                            Current Position
+                        </label>
                         <input
-                            type='text'
+                            type="text"
                             required
-                            name='currentPosition'
-                            className='currentInput'
+                            name="currentPosition"
+                            id="currentPosition"
+                            className="currentInput"
                             value={currentPosition}
                             onChange={(e) => setCurrentPosition(e.target.value)}
                         />
                     </div>
                     <div>
-                        <label htmlFor='currentLength'>For how long? (year)</label>
+                        <label htmlFor="currentLength">
+                            For how many years?
+                        </label>
                         <input
-                            type='number'
+                            type="number"
                             required
-                            name='currentLength'
-                            className='currentInput'
+                            name="currentLength"
+                            id="currentLength"
+                            className="currentInput"
                             value={currentLength}
                             onChange={(e) => setCurrentLength(e.target.value)}
                         />
                     </div>
                     <div>
-                        <label htmlFor='currentTechnologies'>Technologies used</label>
+                        <label htmlFor="currentTechnologies">Tech used</label>
                         <input
-                            type='text'
+                            type="text"
                             required
-                            name='currentTechnologies'
-                            className='currentInput'
+                            name="currentTechnologies"
+                            className="currentInput"
                             value={currentTechnologies}
-                            onChange={(e) => setCurrentTechnologies(e.target.value)}
+                            onChange={(e) =>
+                                setCurrentTechnologies(e.target.value)
+                            }
                         />
                     </div>
+                    <label htmlFor="photo">Upload headshot</label>
+                    <input
+                        type="file"
+                        name="photo"
+                        required
+                        id="photo"
+                        accept="image/x-png,image/jpeg"
+                        onChange={(e) => setHeadshot(e.target.files[0])}
+                    />
+                    <button>Generate</button>
                 </div>
-                <label htmlFor='photo'>Upload your headshot image</label>
-                <input
-                    type='file'
-                    name='photo'
-                    required
-                    id='photo'
-                    accept='image/x-png,image/jpeg'
-                    onChange={(e) => setHeadshot(e.target.files[0])}
-                />
+                {companyInfo.map((company, index) => (
+                    <div className="nestedContainer" key={index}>
+                        <div className="companies">
+                            <label htmlFor="name">Company Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                required
+                                onChange={(e) => handleUpdateCompany(e, index)}
+                            />
+                        </div>
+                        <div className="companies">
+                            <label htmlFor="position">Position Held</label>
+                            <input
+                                type="text"
+                                name="position"
+                                required
+                                onChange={(e) => handleUpdateCompany(e, index)}
+                            />
+                        </div>
 
+                        <div className="btn__group">
+                            {companyInfo.length - 1 === index &&
+                                companyInfo.length < 4 && (
+                                    <button
+                                        id="addBtn"
+                                        onClick={handleAddCompany}
+                                    >
+                                        Add
+                                    </button>
+                                )}
+                            {companyInfo.length > 1 && (
+                                <button
+                                    id="deleteBtn"
+                                    onClick={() => handleRemoveCompany(index)}
+                                >
+                                    Del
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                ))}
                 <button>CREATE RESUME</button>
             </form>
         </div>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,116 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const { Configuration, OpenAIApi } = require("openai");
+const path = require("path");
+// const { config } = require("process");
 const app = express();
 const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+// OpenAI Config
+require('dotenv').config();
+let db = [];
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const GPTFunction = async (text) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: text,
+        temperature: 0.6,
+        max_tokens: 250,
+        top_p: 1,
+        frequency_penalty: 1,
+        presence_penalty: 1,
+    });
+    return response.data.choices[0].text;
+};
+
+// Routes
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 },
+});
 
 app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello World!",
-  });
+    res.json({
+        message: "Hello World!",
+    });
+});
+
+app.post("/resume/create", upload.single("headshotImage"), async (req, res) => {
+    console.log(req.body); // For Postman.
+    const {
+        fullName,
+        currentPosition,
+        currentLength,
+        currentTechnologies,
+        workHistory,
+    } = req.body;
+    const workArray = JSON.parse(workHistory); // Reverts workHistory into its original DStr. from the client.
+    const newEntry = {
+        id: generateID(),
+        fullName,
+        image_url: `http://localhost:4000/uploads/${req.file.filename}`, // be careful of https instead of http!
+        currentPosition,
+        currentLength,
+        currentTechnologies,
+        workHistory: workArray,
+    };
+
+    // 'Converts' workArray into strings for processing
+    const strWorkArray = () => {
+        let string = "I have worked these companies: ";
+        for (let i = 0; i < workArray.length; i++) {
+            string += `${workArray[i].name} as a ${workArray[i].position}. `;
+        }
+        console.log(`strWorkArray -> ${string}`)
+        return string;
+    };
+
+    // Chat-GPT Prompts! The fun part!
+    // deets are some reusable prompt details for the following rather specific prompts to avoid copy pasting.
+    const deets = `I need a professional resume. My details are as follows-- Name: ${fullName}, Role: ${currentPosition} (${currentLength} years).`;
+    const prompt_desc = `${deets} I utilize this technology to assist me with the job: ${currentTechnologies}. Please write me 80 words of summary (in first person writing).`;
+    const prompt_points = `${deets} I excel in this technology: ${currentTechnologies}. Please write me 8 bullet points for the resume on what I am good at.`;
+    const prompt_experience = `${deets} I have professionaly worked in ${workArray.length} companies. ${strWorkArray()} \n Please write me 40 words for each company separated in bullet points of my succession in the company (in first person writing).`;
+
+    // API Call -> Generate GPT-3 Results!
+    const desc = await GPTFunction(prompt_desc);
+    const points = await GPTFunction(prompt_points);
+    const experience = await GPTFunction(prompt_experience);
+    // Place them into an object form
+    const gptData = { desc, points, experience };
+    // console.log(gptData);
+    // Return the results to client
+    const data = { ...newEntry, ...gptData };
+    db.push(data);
+
+    // Send response to client
+    res.json({
+        message: "Request fullfiled.",
+        data,
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
